9d514d92ebf955c5300ec50fe3dcf806b70ffee5
athenaz14 <athenaz11@nycstudents.net>
2026-01-28 09:23:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+
+def right():
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(135)
+t.forward(142)
+right()
+
+turtle.done()
