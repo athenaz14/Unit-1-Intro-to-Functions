9050494888038065fd8d189b644c55fad8920699
athenaz14 <athenaz11@nycstudents.net>
2026-01-29 09:26:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,6 @@ import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
-
-def right():
-t.forward(100)
-t.left(90)
-t.forward(100)
-t.left(135)
-t.forward(142)
-right()
+t.speed(100)
 
 turtle.done()
